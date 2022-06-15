977fd59016244df63fa02bcd8df26eef9821146a
Mariana Palacios <82735874+marianaph6@users.noreply.github.com>
2022-06-15 16:08:11 -0500

    Arithmetic operators use

--- FirstApplication/FirstApplication/Program.cs
+++ FirstApplication/FirstApplication/Program.cs
@@ -2,25 +2,33 @@
 
 namespace FirstApplication
 {
-    //Class
     class Program
     {
-        //Main method 
         static void Main(string[] args)
         {
-            //Sentece or Instruction
-            //Instancia.Metodo
-            Console.WriteLine("Welcome to my Firt Application!"); // Print message to console
+            Console.WriteLine(7 * 5);
+            Console.WriteLine(7 * 5 * 3);
+            Console.WriteLine(5/2); //Se ofrece el resultado del mismo tipo de dato de los operandos
+            Console.WriteLine(5.0 / 2.0); //Se toman como double
+            Console.WriteLine(5 / 2.0); //Entero/Decimal → Decimal
+            Console.WriteLine(9%4);
 
-            int edad; //Declarar
-            edad = 2; //Inicializar
-            Console.WriteLine(edad);
+            int edad = 19;
+
+            Console.WriteLine("Tienes una edad de: "+ edad + " años"); //Concatenación con +
+            Console.WriteLine($"Tienes una edad de: {edad} años"); //Interpolación de String
+
+            //Uso como sufijo
+
+            edad++; //Incremento
+            edad--; //Decremento
+            
+            //Uso como prefijo
+            Console.WriteLine($"Tienes una edad de: {++edad} años"); 
+            Console.WriteLine($"Tienes una edad de: {--edad} años"); 
+
         }
 
-        /*
-         * Esto es un comentario
-         * Esto es otro comentario
-         */
     }
 
 }
