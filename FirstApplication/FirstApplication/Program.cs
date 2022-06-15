016b2fd375ad924882d2512873561a03544558d6
Mariana Palacios <82735874+marianaph6@users.noreply.github.com>
2022-06-15 15:51:13 -0500

    Update/Refactor Hello World with class and method

--- FirstApplication/FirstApplication/Program.cs
+++ FirstApplication/FirstApplication/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace FirstApplication
+{
+    //Class
+    class Program
+    {
+        //Main method 
+        static void Main(string[] args) =>
+            //Sentece or Instruction
+            //Instancia.Metodo
+            Console.WriteLine("Welcome to my Firt Application!");
+    }
+
+}
