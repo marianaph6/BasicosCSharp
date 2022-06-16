ccaa3b22b17331983f82ecf3606b4fe4a37c2b8b
Mariana Palacios <82735874+marianaph6@users.noreply.github.com>
2022-06-16 15:40:42 -0500

    Create class and instances

--- EjemplosPOO/EjemplosPOO/Program.cs
+++ EjemplosPOO/EjemplosPOO/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EjemplosPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Circulo circulo;
+            circulo = new Circulo(); //Iniciación de variable objeto de tipo circulo
+            Console.WriteLine(circulo.calcularArea(47));
+        }
+    }
+
+    class Circulo
+    {
+        double pi = 3.1416;
+
+        public double calcularArea(int radio)
+        {
+            return (radio * pi * pi);
+
+        }
+
+    }
+
+}
