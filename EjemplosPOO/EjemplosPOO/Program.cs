4f18c952c084aa4f2370db00a4b2dcbe17efc121
Mariana Palacios <82735874+marianaph6@users.noreply.github.com>
2022-06-16 15:55:35 -0500

    Encapsulation and modifiers

--- EjemplosPOO/EjemplosPOO/Program.cs
+++ EjemplosPOO/EjemplosPOO/Program.cs
@@ -9,20 +9,42 @@ namespace EjemplosPOO
 
             Circulo circulo;
             circulo = new Circulo(); //Iniciación de variable objeto de tipo circulo
-            Console.WriteLine(circulo.calcularArea(47));
+            Console.WriteLine(circulo.CalcularArea(47));
+
+            ConversorEuroDolar obj = new ConversorEuroDolar();
+
+            obj.cambiarValorEuro(1.45);
+            Console.WriteLine(obj.convierte(50));
         }
     }
 
     class Circulo
     {
-        double pi = 3.1416;
+        private const double pi = 3.1416; //propiedad de la clase
 
-        public double calcularArea(int radio)
+        public double CalcularArea(int radio) // metodo de la clase
         {
             return (radio * pi * pi);
 
         }
 
+    }
+
+    class ConversorEuroDolar
+    {
+        private double euro = 1.253;
+
+        public double convierte(double cantidad) 
+        {
+            return euro * cantidad;
+        
+        }
+
+        public void cambiarValorEuro(double nuevoValor) 
+        {
+            if(nuevoValor < 0) euro = 1.253;
+            else euro = nuevoValor;
+        }
     }
 
 }
