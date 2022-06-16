7c32991c88eb0e533bcfde62212566064c34e4ac
Mariana Palacios <82735874+marianaph6@users.noreply.github.com>
2022-06-16 11:23:11 -0500

    Implement examples methods

--- FirstApplication/FirstApplication/Program.cs
+++ FirstApplication/FirstApplication/Program.cs
@@ -6,35 +6,36 @@ namespace FirstApplication
     {
         static void Main(string[] args)
         {
-            const int VALOR = 5;
-
-            const int VALOR2 = 6;
-
-            // cw+TAB → Console.WriteLine();
-            Console.WriteLine("El valor de la constante es: ", VALOR); //Imprimir con parametros
-
-            Console.WriteLine("El valor de la constante es: {0} ", VALOR, VALOR2); //Imprimir con el parametro 1 
-
-            Console.WriteLine("El valor de la constante es: {1} ", VALOR, VALOR2); //Imprimir con el parametro 1 
-
-            /*
-             * Ejemplo de calculo de area
-             */
 
             const double PI = 3.1416;
 
             Console.WriteLine("Introduce el radio");
 
-            double radio= double.Parse(Console.ReadLine());
-            //double area = radio * radio * PI;
-            double area = Math.Pow(radio,2)* PI;
+            double radio = double.Parse(Console.ReadLine());
+            var area = calcularArea(radio, PI);
+
 
             Console.WriteLine($"El area del circulo es: {area}");
 
-
+            calcularAreaMensaje(radio, PI);
 
         }
 
+
+        static double calcularArea(double radio, double PI)
+        {
+            double area = Math.Pow(radio, 2) * PI;
+            return area;
+        }
+
+
+        static void calcularAreaMensaje(double radio, double PI)
+        {
+            double area = Math.Pow(radio, 2) * PI;
+            Console.WriteLine($"El area del circulo es: {area}");
+        }
+
     }
+
 
 }
