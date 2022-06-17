0240d8b2480545f9ce25f17f218efe820ac8c1f1
Mariana Palacios <82735874+marianaph6@users.noreply.github.com>
2022-06-17 06:44:02 -0500

    Getters and Setters

--- EjemplosPOO/EjemplosPOO/Program.cs
+++ EjemplosPOO/EjemplosPOO/Program.cs
@@ -1,12 +1,13 @@
-﻿using System;
-
-namespace EjemplosPOO
+﻿namespace EjemplosPOO
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Coche coche =  new Coche();
+            Coche coche =  new Coche(4500.25,1200.35);
+
+            coche.setExtras(true,"HOLLLLA");
+            Console.WriteLine(coche.getExtras());
             
         }
 
@@ -30,9 +31,22 @@ namespace EjemplosPOO
             ancho = anchoCoche;
         }
 
+        public void setExtras(bool climatizador, String tapiceria) 
+        {
 
+            this.climatizador = climatizador;
+            this.tapiceria=tapiceria;
 
+            // TODO:
 
+        }
+
+        public String getExtras() 
+        {
+            return $"Extras del coche; \n tapiceria: {tapiceria} \n climatizador: {climatizador} ";
+        }
+
+       
         public String getInfoCoche() 
         {
             return $"Información del coche: Ruedas: {ruedas} Largo: {largo} Ancho: {ancho}";
