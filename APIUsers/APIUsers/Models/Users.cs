fc353358a4cd0ed04a4bdbc13cac45df9461d5da
Mariana Palacios <82735874+marianaph6@users.noreply.github.com>
2022-06-17 13:56:51 -0500

    Create model User and properties

---
+++ APIUsers/APIUsers/Models/Users.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIUsers.Models
+{
+    public class Users
+    {
+        //prop+ double TAB → public int MyProperty { get; set; }
+
+        //Crear modelo de Users, asignar propiedades y data annotations (cuando el modelo pase a ser una tabla)
+
+        [Key]
+        public int Id{ get; set; }
+
+        [Required]
+        public string Name{ get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+    }
+}
