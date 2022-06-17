d09c27d918f8835618090a89026e6689b5da745e
Mariana Palacios <82735874+marianaph6@users.noreply.github.com>
2022-06-17 10:15:12 -0500

    Implement problem and solution Liskov Substitution

--- SOLID/SOLID/Correct/LSP.cs
+++ SOLID/SOLID/Correct/LSP.cs
@@ -8,5 +8,34 @@ namespace SOLID.Correct
 {
     internal class LSP
     {
+        static void Main(string[] args)
+        {
+            Shape shape = new Circle();
+            Console.WriteLine(shape.GetShape());
+            shape = new Triangle();
+            Console.WriteLine(shape.GetShape());
+        }
+    }
+
+    public abstract class Shape
+    {
+        public abstract string GetShape();
+    }
+
+    public class Triangle : Shape
+    {
+        public override string GetShape()
+        {
+            return "Triangle";
+        }
+    }
+
+    public class Circle : Triangle
+    {
+        public override string GetShape()
+        {
+            return "Circle";
+        }
     }
 }
+
--- SOLID/SOLID/Wrong/LSP.cs
+++ SOLID/SOLID/Wrong/LSP.cs
@@ -4,9 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SOLID.Correct
+namespace SOLID.Wrong
 {
-    internal class LSP
+    internal class LSPWrong
     {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Circle();
+            Console.WriteLine(triangle.GetColor());
+        }
+    }
+
+    public class Triangle
+    {
+        public virtual string GetShape()
+        {
+            return "Triangle";
+        }
+
+        internal bool GetColor()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Circle : Triangle
+    {
+        public override string GetShape()
+        {
+            return "Circle";
+        }
     }
 }
+
