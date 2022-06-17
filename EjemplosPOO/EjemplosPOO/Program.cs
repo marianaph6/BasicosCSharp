9e120f77f4206e5e1fb56f508359e893d28f03e9
Mariana Palacios <82735874+marianaph6@users.noreply.github.com>
2022-06-17 07:15:36 -0500

    Call methods and use of Math

--- EjemplosPOO/EjemplosPOO/Program.cs
+++ EjemplosPOO/EjemplosPOO/Program.cs
@@ -4,58 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Coche coche =  new Coche(4500.25,1200.35);
 
-            coche.setExtras(true,"HOLLLLA");
-            Console.WriteLine(coche.getExtras());
+            realizarTarea();
             
         }
 
+        static void realizarTarea()
+        {
+            Punto origen = new Punto();
+            Punto destino = new Punto(128, 80);
+
+            double distancia= origen.DistanciaHasta(destino);
+
+            Console.WriteLine($"La distancia entre los puntos es: {distancia}");
+        }
     }
 
-    class Coche 
-    {
-        //Dar estado inicial a los objetos de tipo coche
-        public Coche() 
-        {
-            ruedas = 4;
-            largo = 2300.5;
-            ancho = 0.800;
-        }
 
-        //Dar estado inicial a los objetos de tipo coche con parametros
-        public Coche(double largoCoche, double anchoCoche)
-        {
-            ruedas = 4;
-            largo = largoCoche;
-            ancho = anchoCoche;
-        }
-
-        public void setExtras(bool climatizador, String tapiceria) 
-        {
-
-            this.climatizador = climatizador;
-            this.tapiceria=tapiceria;
-
-            // TODO:
-
-        }
-
-        public String getExtras() 
-        {
-            return $"Extras del coche; \n tapiceria: {tapiceria} \n climatizador: {climatizador} ";
-        }
-
-       
-        public String getInfoCoche() 
-        {
-            return $"Información del coche: Ruedas: {ruedas} Largo: {largo} Ancho: {ancho}";
-        }
-        private int ruedas;
-        private double largo, ancho;
-        private bool climatizador;
-        private String tapiceria;
-    }
 
 
 
---
+++ EjemplosPOO/EjemplosPOO/Punto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemplosPOO
+{
+    internal class Punto
+    {
+        public Punto(int x, int y) 
+        {
+            //This → propiedades y metodos que pertenecen a la clase punto
+            this.x = x; 
+            this.y = y; 
+        }
+
+        public Punto() 
+        {
+            this.x = 0;
+            this.y = 0; 
+        }
+
+        public double DistanciaHasta(Punto otroPunto) 
+        {
+            int xDif= this.x - otroPunto.x;
+            int yDif= this.y - otroPunto.y;
+
+            double distanciaPuntos = Math.Sqrt(Math.Pow(xDif, 2) + Math.Pow(yDif, 2));
+
+            return distanciaPuntos;
+        }
+
+        private int x, y;
+    }
+}
