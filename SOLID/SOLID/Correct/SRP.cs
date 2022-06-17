4a3fc632fed5b58090af64f1c575410dceb1b3fc
Mariana Palacios <82735874+marianaph6@users.noreply.github.com>
2022-06-17 10:05:42 -0500

    Implement problem and solution SRP (Single Responsability)

--- SOLID/SOLID/Correct/SRP.cs
+++ SOLID/SOLID/Correct/SRP.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace SOLID.Correct
 {
-    internal class SRP
+    internal class SRPCorrect
+    {
+        public class GarageStation
+        {
+            IGarageUtility _garageUtil;
+
+            public GarageStation(IGarageUtility garageUtil)
+            {
+                this._garageUtil = garageUtil;
+            }
+            public void OpenForService()
+            {
+                _garageUtil.OpenGate();
+            }
+            public void DoService()
+            {
+                //Check if service station is opened and then
+                //finish the vehicle service
+            }
+            public void CloseGarage()
+            {
+                _garageUtil.CloseGate();
+            }
+        }
+        public class GarageStationUtility : IGarageUtility
+        {
+            public void OpenGate()
+            {
+                //Open the Garage for service
+            }
+
+            public void CloseGate()
+            {
+                //Close the Garage functionlity
+            }
+        }
+
+        public interface IGarageUtility
+        {
+            void OpenGate();
+            void CloseGate();
+        }
+    }
+
+    public class Vehicle
     {
     }
 }
--- SOLID/SOLID/Wrong/SRP.cs
+++ SOLID/SOLID/Wrong/SRP.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using SOLID.Correct;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SOLID.Correct
+namespace SOLID.Wrong
 {
-    internal class SRP
+    internal class SRPWrong
     {
+        public class GarageStation
+        {
+            public void DoOpenGate()
+            {
+                //Open the gate functinality
+            }
+
+            public void PerformService(Vehicle vehicle)
+            {
+                //Check if garage is opened
+                //finish the vehicle service
+            }
+
+            public void DoCloseGate()
+            {
+                //Close the gate functinality
+            }
+        }
     }
 }
