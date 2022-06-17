0e0a5c541f12914a5568b99fb3506c96fd3d7784
Mariana Palacios <82735874+marianaph6@users.noreply.github.com>
2022-06-17 10:08:17 -0500

    Implement and solution OCP (Open Closed)

--- SOLID/SOLID/Wrong/OCP.cs
+++ SOLID/SOLID/Wrong/OCP.cs
@@ -4,9 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SOLID.Correct
+namespace SOLID.Wrong
 {
-    internal class OCP
+    internal class OCPWrong
     {
+        public class Account
+        {
+            public decimal Interest { get; set; }
+            public decimal Balance { get; set; }
+            // members and function declaration
+            public decimal CalcInterest(string accType)
+            {
+
+                if (accType == "Regular") // savings
+                {
+                    Interest = (Balance * 4) / 100;
+                    if (Balance < 1000) Interest -= (Balance * 2) / 100;
+                    if (Balance < 50000) Interest += (Balance * 4) / 100;
+                }
+                else if (accType == "Salary") // salary savings
+                {
+                    Interest = (Balance * 5) / 100;
+                }
+                else if (accType == "Corporate") // Corporate
+                {
+                    Interest = (Balance * 3) / 100;
+                }
+                return Interest;
+            }
+        }
     }
 }
