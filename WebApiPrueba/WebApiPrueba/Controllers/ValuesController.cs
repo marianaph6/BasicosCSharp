407842eeaa18706da57dc0675a6800080c8c268f
Mariana Palacios <82735874+marianaph6@users.noreply.github.com>
2022-06-20 21:34:16 -0500

    Create project WebApiDePrueba

---
+++ WebApiPrueba/WebApiPrueba/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiPrueba.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController: ControllerBase
+    {
+        //GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        //GET api/values/5
+        public ActionResult<string> Get(int id)
+        {
+            return "value1";
+        }
+
+
+        //POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value) 
+        {
+
+        }
+
+        //PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) 
+        { }
+    }
+}
